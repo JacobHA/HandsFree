a33a212596d871244a712ba45780abffc05fa610
Jacob Adamczyk <42879357+JacobHA@users.noreply.github.com>
2022-06-02 19:56:51 -0400

    working on cross sections

--- cross_section.py
+++ cross_section.py
@@ -16,6 +16,7 @@ import mediapipe as mp
 from vedo import *
 from time import sleep
 from google.protobuf.json_format import MessageToDict
+import time
 
 import numpy as np
 from utils import *
@@ -27,18 +28,26 @@ z_unit_vec = np.array([0, 0, 1])
 STL_name = r'A.stl'
 
 v = Mesh(STL_name)
-
 avg_model_size = v.averageSize()
 v.scale(avg_model_size * 0.00001)
 dim_scale = np.mean(v.scale())*100
 print(avg_model_size)
-cam = dict(pos=(1,0,0), focalPoint=(0,0,0), viewup=(0,0,1))
+cam = dict(pos=(1,0,0), focalPoint=(0,0,0), viewup=(1,1,1))
 
 # Define a plane that goes through the origin and is parallel to the xy-plane
 # plane = plane(pos=(0,0,0), normal=(0,0,1))
 # Plot this plane
 
-pl = Plane(v.centerOfMass(), normal=[0,1,0], s=[dim_scale,dim_scale], c='black', alpha=0.3)
+plt = show(v, axes=4, viewup='z', camera=cam, interactive=False)
+zero = v.centerOfMass()
 
-plt = show(v, pl, axes=4, viewup='z', camera=cam, interactive=True)
+ax = np.linspace(-0.05,0.00,100)
+for a in ax:
+    print(a)
+    orig = zero + [0,a,0]
+    v.cutWithPlane(origin=orig, normal=[0,1,0])
+    pl = Plane(orig, normal=[0,1,0], s=[dim_scale,dim_scale], c='black', alpha=0.7)
 
+    plt.show(v, pl, axes=4, viewup='z', camera=cam, interactive=False)
+
+plt.show(v, pl, axes=4, viewup='z', camera=cam, interactive=True)
