cb5559d91da37a77e44803ba6ff160cc2bd511f5
Jacob Adamczyk <42879357+JacobHA@users.noreply.github.com>
2022-06-06 13:21:50 -0400

    added some keystroke commands

--- utils.py
+++ utils.py
@@ -12,6 +12,7 @@ class ObjectDisplayer:
         self.axes = axes
         self.init_scale = init_scale
         self.camera = camera
+        self.is_locked = False
         self.object = Mesh(filename)
         self.status_message = Text2D(initial_msg, pos="top-center", font=2, c='w', bg='b3', alpha=1)
 
@@ -19,21 +20,25 @@ class ObjectDisplayer:
 
     # Display function
     def show_object(self, new_object=None):
-        if new_object is None:
-            new_object = self.object
+        if not self.is_locked:
+            if new_object is None:
+                new_object = self.object
 
-        _ = show(new_object, self.status_message, axes=self.axes, viewup='z', camera=self.camera, interactive=False)
-
+            _ = show(new_object, self.status_message, axes=self.axes, viewup='z', camera=self.camera, interactive=False)
+        else:
+            # There is probably a smarter way to do this so that we do not have to calculate things all the time
+            pass
 
     # Initial functions
-    def initial_rescale(self):
+    def initial_setup(self):
         # Do fresh import of object
         self.object = Mesh(self.filename)
         model_size = self.object.averageSize()
         self.object.scale(self.init_scale * model_size)
+        self.object.shift(-self.object.centerOfMass())
         
     def initial_display(self):
-        self.initial_rescale()
+        self.initial_setup()
         # self.initial_shift()
         self.show_object()
 
@@ -56,7 +61,15 @@ class ObjectDisplayer:
         self.object.rotate(angle=angle, axis=axis, point=point)
         self.show_object()
 
-        
+    # Keyboard functions:
+    def lock(self):
+        self.is_locked = not self.is_locked #True
+        msg = 'Locked (press \"l\" again to unlock)' if self.is_locked else 'Unlocked'
+        self.update_message(msg)
+        _ = show(self.object, self.status_message, axes=self.axes, viewup='z', camera=self.camera, interactive=False)
+
+
+
 # def init_show(STL_name, rescale, msg='', camera=cam):
 #     status_message = Text2D(msg, pos="top-center", font=2, c='w', bg='b3', alpha=1)
 
